a58c3aefc02deb1116f6f27cf5b80ca5f34cfb8d
bblanche <brendatonleu@gmail.com>
2023-06-26 12:10:47 +0200

    data sac a dos

---
+++ read_data.jl
@@ -0,0 +1,28 @@
+function readKnaptxtInstance(filename)
+    price=[]
+    weight=[]
+    KnapCap=[]
+    open(filename) do f
+        for i in 1:3
+            tok = split(readline(f))
+            if(tok[1] == "ListPrices=")
+                for i in 2:(length(tok)-1)
+                    push!(price,parse(Int64, tok[i]))
+                end
+            elseif(tok[1] == "ListWeights=")
+                for i in 2:(length(tok)-1)
+                    push!(weight,parse(Int64, tok[i]))
+                end
+            elseif(tok[1] == "Capacity=")
+                push!(KnapCap, parse(Int64, tok[2]))
+            else
+                println("Unknown read :", tok)
+            end 
+        end
+    end
+    capacity=KnapCap[1]
+    return price, weight, capacity
+end
+
+
+price, weight, capacity_max = readKnaptxtInstance(filename)
