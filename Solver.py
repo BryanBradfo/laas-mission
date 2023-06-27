1e7a2cf8554716fe7a127a1c1b1be3e4237b62cd
Alice Devilder <alice.devilder@gmail.com>
2023-06-27 17:42:28 +0200

    Solver modified

--- Solver.py
+++ Solver.py
@@ -1,4 +1,6 @@
-from docplex.cp.model import CpoModel, CpoSolver
+from docplex.cp.model import *
+from docplex.cp.config import get_default
+import time
 
 class Solver:
 
@@ -18,9 +20,42 @@ class Solver:
         self._constraints.append(constraint)
 
 
-    def solve_init(self, model, k):
-    
-      
+    def solve(self, k, n, m, T_machine, T_duration, duration):
+
+        model = CpoModel() 
+
+        tps1 = time.time()
+
+        # --------- Create the model variables 
+        print("\nCreating the model variables...")
+        # Create task interval variables and add them to the solver
+
+        variables = [[None for j in range(m)] for i in range(n)]
+        for i in range(n):
+            for j in range(m):
+                # print(duration[i][j])
+                variables[i][j] = model.interval_var(size=int(duration[i][j]), name="T{}-{}".format(i,j))
+
+        # ------------ Add variables to the solver
+        self.set_variables(variables)
+        print("Model variables created !")
+
+        # ------------ Add constraints to the solver
+
+        print("\nAdding precedence constraints to the solver...")
+        # Add precedence constraints
+        for i in range(n):
+            for j in range(1,m):
+                # print(variables[i][T_machine[i*m + j-1]])
+                # print(variables[i][T_machine[i*m + j]])
+                self.add_constraint(end_before_start(variables[i][T_machine[i*m + j-1]], variables[i][T_machine[i*m + j]]))
+        print("Precedence constraints added !")
+
+        print("\nAdding disjunctive constraints to the solver...")
+        # Add disjunctive constraints 
+        for machine in range(m):
+            self.add_constraint(no_overlap([variables[i][machine] for i in range(n)]))
+        print("Disjunctive constraints added !")
 
         # Add the constraints
         for constraint in self._constraints:
@@ -43,9 +78,11 @@ class Solver:
         list_sol = []
         for sol in msol:
             list_sol.append(sol)
+
+        tps2 = time.time()
                 
         # Renvoie les k premieres solutions (makespan)
 
         # return solver, status
-        return list_sol[:k], nb_solution
+        return list_sol[:k], nb_solution, tps2 - tps1
         
