3bd1655f055324fa6c4dd8cadb7c1788bc427033
Alice Devilder <alice.devilder@gmail.com>
2023-06-28 14:53:27 +0200

    New file

--- User.py
+++ User.py
@@ -41,3 +41,4 @@ class User:
 
     def getPreferences(self):
         return self.preferences
+    
---
+++ test.py
@@ -0,0 +1,3 @@
+
+#Vérifier le résultat des preférences renvoyés par la fonction classerSolutions
+def test_preferences(list_pref)
