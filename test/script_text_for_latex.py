3ca4b4b2c6f1929edadb1f22ff563f2b41ea671c
Alice Devilder <alice.devilder@gmail.com>
2023-08-04 14:58:41 +0200

    Ajout des valeurs de optimalval pour les différents fichiers

--- test/script_text_for_latex.py
+++ test/script_text_for_latex.py
@@ -25,23 +25,24 @@ def ligne_latex(values):
 def main():
     #-----------------------------Parameters of every methods-----------------------------#
     #Names of files must be different
-    # list_files = ['../file_with_optimal_val/la01.txt', '../file_with_optimal_val/la02.txt', '../file_with_optimal_val/la03.txt', '../file_with_optimal_val/la04.txt', '../file_with_optimal_val/la05.txt', '../file_with_optimal_val/la06.txt' ]
+    list_files = ['../file_with_optimal_val/la04.txt', '../file_with_optimal_val/la05.txt', '../file_with_optimal_val/la06.txt', '../file_with_optimal_val/la07.txt',  '../file_with_optimal_val/la08.txt', 
+                  '../file_with_optimal_val/la09.txt', '../file_with_optimal_val/la10.txt', '../file_with_optimal_val/la11.txt','../file_with_optimal_val/la12.txt' ]
     # list_files = ['../file_with_optimal_val/la01.txt', '../file_with_optimal_val/la02.txt', '../file_with_optimal_val/la03.txt']
-    list_files = ['../file_with_optimal_val/la01.txt', '../file_with_optimal_val/la02.txt', '../file_with_optimal_val/la03.txt', '../file_with_optimal_val/la04.txt', '../file_with_optimal_val/la05.txt', 
-                '../file_with_optimal_val/la06.txt',  '../file_with_optimal_val/la07.txt',  '../file_with_optimal_val/la08.txt', '../file_with_optimal_val/la09.txt', '../file_with_optimal_val/la10.txt',
-                '../file_with_optimal_val/la11.txt','../file_with_optimal_val/la12.txt','../file_with_optimal_val/la13.txt','../file_with_optimal_val/la14.txt','../file_with_optimal_val/la15.txt',
-                '../file_with_optimal_val/la16.txt','../file_with_optimal_val/la17.txt','../file_with_optimal_val/la18.txt','../file_with_optimal_val/la19.txt','../file_with_optimal_val/la20.txt',
-                '../file_with_optimal_val/la21.txt','../file_with_optimal_val/la22.txt','../file_with_optimal_val/la23.txt','../file_with_optimal_val/la24.txt','../file_with_optimal_val/la25.txt',
-                '../file_with_optimal_val/la26.txt','../file_with_optimal_val/la27.txt','../file_with_optimal_val/la28.txt','../file_with_optimal_val/la29.txt','../file_with_optimal_val/la30.txt',
-                '../file_with_optimal_val/la31.txt','../file_with_optimal_val/la32.txt','../file_with_optimal_val/la33.txt','../file_with_optimal_val/la34.txt','../file_with_optimal_val/la35.txt',
-                '../file_with_optimal_val/la36.txt','../file_with_optimal_val/la37.txt','../file_with_optimal_val/la38.txt','../file_with_optimal_val/la39.txt', '../file_with_optimal_val/la40.txt'
-                ]
+    # list_files = ['../file_with_optimal_val/la01.txt', '../file_with_optimal_val/la02.txt', '../file_with_optimal_val/la03.txt', '../file_with_optimal_val/la04.txt', '../file_with_optimal_val/la05.txt', 
+                # '../file_with_optimal_val/la06.txt',  '../file_with_optimal_val/la07.txt',  '../file_with_optimal_val/la08.txt', '../file_with_optimal_val/la09.txt', '../file_with_optimal_val/la10.txt',
+                # '../file_with_optimal_val/la11.txt','../file_with_optimal_val/la12.txt','../file_with_optimal_val/la13.txt','../file_with_optimal_val/la14.txt','../file_with_optimal_val/la15.txt',
+                # '../file_with_optimal_val/la16.txt','../file_with_optimal_val/la17.txt','../file_with_optimal_val/la18.txt','../file_with_optimal_val/la19.txt','../file_with_optimal_val/la20.txt',
+                # '../file_with_optimal_val/la21.txt','../file_with_optimal_val/la22.txt','../file_with_optimal_val/la23.txt','../file_with_optimal_val/la24.txt','../file_with_optimal_val/la25.txt',
+                # '../file_with_optimal_val/la26.txt','../file_with_optimal_val/la27.txt','../file_with_optimal_val/la28.txt','../file_with_optimal_val/la29.txt','../file_with_optimal_val/la30.txt',
+                # '../file_with_optimal_val/la31.txt','../file_with_optimal_val/la32.txt','../file_with_optimal_val/la33.txt','../file_with_optimal_val/la34.txt','../file_with_optimal_val/la35.txt',
+                # '../file_with_optimal_val/la36.txt','../file_with_optimal_val/la37.txt','../file_with_optimal_val/la38.txt','../file_with_optimal_val/la39.txt', '../file_with_optimal_val/la40.txt'
+                # ]
     list_plot_name = ['la0' + str(i) for i in range (1,len(list_files)+1)]
     list_nb_layers = [5 for i in range (len(list_files))]
     list_k = [20 for i in range (len(list_files))]
     list_k_k = [15 for i in range (len(list_files))]
-    list_tps_max = [6000 for i in range (len(list_files))]
-    list_it_max = [400 for i in range (len(list_files))]
+    list_tps_max = [2000 for i in range (len(list_files))]
+    list_it_max = [200 for i in range (len(list_files))]
     list_type_operation = ['plus' for i in range (len(list_files))]
     list_type_user = ["user_reg" for i in range (len(list_files))]
     # list_type_user = ["user_reg", "user_reg", "user_reg", "user_reg", "user_reg"]
@@ -53,7 +54,7 @@ def main():
     #-----------------------------Waiting results-----------------------------#
     #optimal_value_regularity[0] --> type_operation = "plus"
     #optimal_value_regularity[1] --> type_operation = "fois"
-    optimal_value_regularity = [[1341, 1455, 1486, 1224, 737, 2811], [2952432, 5052960, 3455230]]
+    optimal_value_regularity = [[1341, 1455, 1486, 1224, 737, 2811, 2026, 1542, 1560, 2006, 4960, 2324, 2594, 3540, 6216, 19459, 13866, 15630, 18390, 16451, 30553, 30039, la23, la24, 25841, 26482, 23883, 34420, 42152, 38370], [2952432, 5052960, 3455230]]
     optimal_value_simple = [666, 655, 597, 590, 593,
                             926, 890, 863, 951, 958,
                             1222, 1039, 1150, 1292, 1207,
